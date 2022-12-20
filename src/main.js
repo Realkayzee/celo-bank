0eea78dbb18ff042ca6e22bd26574c8c4853017b
kayzee <olaniyikazeem112@gmail.com>
2022-12-20 23:46:33 +0100

    set up js

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import openCity from "./tab";
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
+import BigNumber from "bignumber.js";
+import CELOBANK from "../contract/celobank.abi.json";
